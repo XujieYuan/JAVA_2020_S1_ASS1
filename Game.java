1f8409307243d1a396b8f21cb8ae5d003e5498a6
Arthur <55334284+XujieYuan@users.noreply.github.com>
2020-04-09 15:38:05 +0800

    Add files via upload

--- Game.java
+++ Game.java
@@ -10,14 +10,14 @@ public class Game {
     /**
      *
      */
-    public Game(){
+    public Game() {
         cardGamePlayer = new Player();
     }
 
     /**
      *
      */
-    public Game(Player newCardGamePlayer){
+    public Game(Player newCardGamePlayer) {
         cardGamePlayer = newCardGamePlayer;
     }
 
@@ -38,28 +38,33 @@ public class Game {
     /**
      *
      */
-    public void start(){
+    public void start() {
         displayWelcomeMessage();
         registerPlayer();
-        Card cd = new Card();
-        Card card = cd.creatCard();
-        compareSuit(card);
-        //compareNumber(card);
+        playOnce();
         System.exit(0);
     }
 
-    public void displayWelcomeMessage(){
+    public void playOnce()
+    {
+        int gameTime = 0;
+        Card cd = new Card();
+        Card card = cd.creatCard();
+        compareSuit(card);
+        compareNumber(card);
+        playAgain();
+    }
+
+    public void displayWelcomeMessage() {
         System.out.println("Welcome to Guess a Card Game!");
     }
 
-    public Player registerPlayer(){
+    public Player registerPlayer() {
         System.out.println("Please enter your name!");
         Scanner console = new Scanner(System.in);
         String name = console.nextLine();
-        for(int i = 0; i < name.length(); i++)
-        {
-            while (!((name.charAt(i) >= 'a' && name.charAt(i) <= 'z') || (name.charAt(i) >= 'A' && name.charAt(i) <= 'Z')))
-            {
+        for (int i = 0; i < name.length(); i++) {
+            while (!((name.charAt(i) >= 'a' && name.charAt(i) <= 'z') || (name.charAt(i) >= 'A' && name.charAt(i) <= 'Z'))) {
                 System.out.println("The player’s name cannot be blank and can contain only alphabetic characters.");
                 name = console.nextLine();
             }
@@ -68,51 +73,36 @@ public class Game {
         return cardGamePlayer;
     }
 
-
-
-    public void compareSuit(Card card)
-    {
-        int attemptTime = 1;
-        int wrongTime = 0;
-
-        while (attemptTime < 3)
-        {
+    public void compareSuit(Card card) {
+        int wrongTime = 3;
+        for (int i = 0; i < 3; i++) {
             enterSuit();
-            if (cardGamePlayer.getGuess() != card.getSuit())
-            {
-                System.out.println("Incorrect guess!");
-                System.out.println(" Wrong ATTEMPT " + attemptTime);
-                wrongTime++;
-                attemptTime++;
-                enterSuit();
-            }
-            if (cardGamePlayer.getGuess() == card.getSuit())
-            {
+            if (cardGamePlayer.getGuess() == card.getSuit()) {
                 System.out.println("Congratulations! This is a correct guess!");
+                break;
+            } else if ((cardGamePlayer.getGuess() != card.getSuit()) && (wrongTime != 1)) {
+                System.out.println("Incorrect guess!");
+                System.out.println("You still have " + (wrongTime - 1) + " chances.");
+                System.out.println();
+                wrongTime--;
+            } else if ((cardGamePlayer.getGuess() != card.getSuit()) && (wrongTime == 1)) {
+                System.out.println("You did 3 incorrect guesses. The correct suit is " + card.getSuit() + ".");
+                wrongTime--;
                 break;
             }
         }
-        if (wrongTime == 1)
+        if (wrongTime == 2)
             cardGamePlayer.setScore(cardGamePlayer.getScore() - 5);
-        else if (wrongTime == 2)
+        else if (wrongTime == 1)
             cardGamePlayer.setScore(cardGamePlayer.getScore() - 15);
-        else if (wrongTime == 3)
-        {
+        else if (wrongTime == 0) {
             cardGamePlayer.setScore(cardGamePlayer.getScore() - 30);
-            System.out.println("You did 3 incorrect guesses.");
         }
-        System.out.println("Your current score is " + cardGamePlayer.getScore());
+        System.out.println("Your current score is " + cardGamePlayer.getScore() + "!");
+        System.out.println();
     }
 
-    public boolean checkCorrectSuitGuess(int suit)
-    {
-        if (cardGamePlayer.getGuess() == suit)
-            return true;
-        else
-            return false;
-    }
-
-    public void enterSuit()
+    public void enterSuit()//Can add IOException in this method
     {
         System.out.println("Please enter a suit.");
         System.out.println("Press 1 for H (Hearts)");
@@ -121,54 +111,103 @@ public class Game {
         System.out.println("Press 4 for S (Spades)");
         Scanner console = new Scanner(System.in);
         int suit = console.nextInt();
-        while (suit < 1 || suit > 4)
-        {
+        while (suit < 1 || suit > 4) {
             System.out.println("Invalid value! Your choice must between 1-4! Please enter again!");
             suit = console.nextInt();
         }
         cardGamePlayer.setGuess(suit);
-        System.out.println("Your guess is " + cardGamePlayer.getGuess());
+        System.out.println("Your guess is " + cardGamePlayer.getGuess() + ".");
     }
 
-    /**
-    public void compareNumber(Card card)
-    {
-        int attemptTime = 0;
-        enterSuit();
-        while ((cardGamePlayer.getGuess() != card.getSuit()) && (attemptTime <= 3))
-        {
-            System.out.println("Incorrect guess!");
-            attemptTime++;
-            enterSuit();
+    public void compareNumber(Card card) {
+        int wrongTime = 4;
+        for (int i = 0; i < 4; i++) {
+            enterCardNumber();
+            if (cardGamePlayer.getGuess() == card.getNumber()) {
+                System.out.println("Congratulations! This is a correct guess! !WIN!");
+                if (checkScore())
+                    break;
+            } else if ((cardGamePlayer.getGuess() != card.getNumber()) && (wrongTime != 1)) {
+                System.out.println("Incorrect guess!");
+                System.out.println("You still have " + (wrongTime - 1) + " chances.");
+                wrongTime--;
+                if (wrongTime == 3) {
+                    cardGamePlayer.setScore(cardGamePlayer.getScore() - 2);
+                } else if (wrongTime == 2) {
+                    cardGamePlayer.setScore(cardGamePlayer.getScore() - 6);
+                } else if (wrongTime == 1) {
+                    cardGamePlayer.setScore(cardGamePlayer.getScore() - 12);
+                }
+                if (checkScore()) {
+                    if (card.getNumber() > cardGamePlayer.getGuess())
+                    {
+                        System.out.println("The correct card number is higher than your last guess.");
+                    }
+                    if (card.getNumber() < cardGamePlayer.getGuess())
+                    {
+                        System.out.println("The correct card number is lower than your last guess.");
+                    }
+                    continue;
+                }
+                if (!(checkScore())) {
+                    System.out.println("But your score is not greater than 0 now! !LOSE!");
+                    cardGamePlayer.setScore(0);
+                    break;
+                }
+            } else if ((cardGamePlayer.getGuess() != card.getNumber()) && (wrongTime == 1)) {
+                cardGamePlayer.setScore(cardGamePlayer.getScore() - 20);
+                System.out.println("!LOSE! You did 4 incorrect guesses. The correct number is " + card.getNumber() + ".");//Can write a switch method in Class Card to transfer the number to character
+                cardGamePlayer.setScore(0);
+                break;
+            }
         }
-        if (cardGamePlayer.getGuess() == card.getSuit())
-        {
-            System.out.println("Congratulations! This is a correct guess!");
-        }
-        if (attemptTime == 0)
-            cardGamePlayer.setScore(cardGamePlayer.getScore() - 0);
-        else if (attemptTime == 1)
-            cardGamePlayer.setScore(cardGamePlayer.getScore() - 5);
-        else if (attemptTime == 2)
-            cardGamePlayer.setScore(cardGamePlayer.getScore() - 15);
-        else
-            cardGamePlayer.setScore(cardGamePlayer.getScore() - 30);
-        System.out.println("Your current score is " + cardGamePlayer.getScore());
+        System.out.println("Your current score is " + cardGamePlayer.getScore() + "!");
+        System.out.println();
     }
-     */
 
-    public void enterCardNumber()
+    public void enterCardNumber()//Can add IOException in this method
     {
         System.out.println("Please enter a card number from 1 to 13.");
         Scanner console = new Scanner(System.in);
         int cardNumber = console.nextInt();
-        while (cardNumber < 1 || cardNumber > 13)
-        {
+        while (cardNumber < 1 || cardNumber > 13) {
             System.out.println("Invalid value! Your choice must between 1-13! Please enter again!");
             cardNumber = console.nextInt();
         }
         cardGamePlayer.setGuess(cardNumber);
     }
 
+    public boolean checkScore() {
+        if (cardGamePlayer.getScore() > 0)
+            return true;
+        else
+            return false;
+    }
 
+    public boolean playAgain() {
+        boolean whetherPlayAgain = true;
+        System.out.println("Do you want to play another game?");
+        System.out.println("Press 'y' for 'Yes'");
+        System.out.println("Press 'n' for 'No'");
+        Scanner console = new Scanner(System.in);
+        String choice = console.nextLine();
+        switch (choice) {
+            case "y":
+                playOnce();
+                break;
+            case "n":
+                endGame();
+                whetherPlayAgain = false;
+                break;
+            default:
+                System.out.println("Invalid option! Please enter again!");
+                break;
+        }
+        return whetherPlayAgain;
+    }
+
+    public void endGame()
+    {
+
+    }
 }
